583a21e535a129a99b4509f045358d197f7849f1
Temsar-626 <93128305+Temsar-626@users.noreply.github.com>
2024-03-20 16:29:58 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 Bot_name = "Temsar uploader"
-api_id = 25478159
-api_hash = "17a40408fc28428c884a5c16202b2794"
+api_id = 25478150
+api_hash = "17a50408fc28428c884a5c16202b2794"
 Token = "6834947843:AAGlLrSfItSNZze8RV4rD8pBx81Eafgy0ak"
-chanel1_id= -1002082050308
+chanel1_id= -1002082050309
 chanel1_Join_link = "https://t.me/+ZxAq42CdcOk2OTY0"
